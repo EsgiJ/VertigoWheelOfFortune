7a982366e873455a76f4e0793ab397fef2da6c22
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2026-05-06 19:23:41 +0300

    feat: add BuildSlices method to Wheel script to configure slices at the start of the game

--- Assets/_Project/Scripts/Wheel.cs
+++ Assets/_Project/Scripts/Wheel.cs
@@ -1,24 +1,55 @@
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using System.Collections.Generic;
 
 namespace WheelOfFortune.Wheel
 {
     public class Wheel : MonoBehaviour
     {
         [SerializeField] private RectTransform _wheelBase;
+        [SerializeField] private RectTransform _wheelAnchorRotor;
+        [SerializeField] private WheelSlice _slicePrefab;
         [SerializeField] private Button _spinButton;
+        [SerializeField] private Sprite[] _icons;
 
+        [SerializeField] private int _sliceCount = 8;
+        [SerializeField] private float _sliceRadius = 140f; // Distance from center to slice position
         [SerializeField] private float _spinDuration = 4f;
         [SerializeField] private int _minSpinRounds= 3;
+        [SerializeField] private int[] _amounts;
 
+        private List<WheelSlice> _slices = new List<WheelSlice>();
         private bool _isSpinning = false;
 
         void Awake()
         {
+            BuildSlices();
             _spinButton.onClick.AddListener(Spin);
         }
         
+        private void BuildSlices()
+        {
+            float anglePerSlice = 360f / _sliceCount;
+
+            for(int i = 0; i < _sliceCount; i++)
+            {
+                WheelSlice slice = Instantiate(_slicePrefab, _wheelAnchorRotor);
+                slice.name = $"Slice_ {i}";
+
+                float angle = -i * anglePerSlice;
+                float rad = (angle + 90f) * Mathf.Deg2Rad; // +90 to start from top 
+
+                RectTransform rectTransform = slice.transform as RectTransform;
+                rectTransform.anchoredPosition = new Vector2(Mathf.Cos(rad) * _sliceRadius, Mathf.Sin(rad) * _sliceRadius);
+
+                Sprite icon = _icons[i];
+                int amount = _amounts[i];
+                slice.Initialize(i, icon, amount);
+
+                _slices.Add(slice);
+            }
+        }
         private void Spin()
         {
             if(_isSpinning)
