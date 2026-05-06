9aafc07b33f07839b5b0d5c435c05785bd6b791c
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2026-05-06 18:11:18 +0300

    refactor: wrap wheel script in namespace

--- Assets/_Project/Scripts/Wheel.cs
+++ Assets/_Project/Scripts/Wheel.cs
@@ -2,39 +2,42 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 
-public class Wheel : MonoBehaviour
+namespace WheelOfFortune.Wheel
 {
-    [SerializeField] private RectTransform _wheelBase;
-    [SerializeField] private Button _spinButton;
-
-    [SerializeField] private float _spinDuration = 4f;
-    [SerializeField] private int _minSpinRounds= 3;
-
-    private bool _isSpinning = false;
-
-    void Awake()
+    public class Wheel : MonoBehaviour
     {
-        _spinButton.onClick.AddListener(Spin);
-    }
-    
-    private void Spin()
-    {
-        if(_isSpinning)
-            return;
+        [SerializeField] private RectTransform _wheelBase;
+        [SerializeField] private Button _spinButton;
 
-        _isSpinning = true;
-        _spinButton.interactable = false;
+        [SerializeField] private float _spinDuration = 4f;
+        [SerializeField] private int _minSpinRounds= 3;
 
-        float randomEndAngle = Random.Range(0f, 360f);
-        float total = _minSpinRounds * 360f + randomEndAngle;
+        private bool _isSpinning = false;
 
-        _wheelBase.transform.DORotate(new Vector3(0f, 0f, -total), _spinDuration, RotateMode.FastBeyond360)
-            .SetEase(Ease.OutCubic)
-            .SetLink(gameObject)
-            .OnComplete(() =>
-            {
-               _isSpinning = false;
-               _spinButton.interactable = true; 
-            });
+        void Awake()
+        {
+            _spinButton.onClick.AddListener(Spin);
+        }
+        
+        private void Spin()
+        {
+            if(_isSpinning)
+                return;
+
+            _isSpinning = true;
+            _spinButton.interactable = false;
+
+            float randomEndAngle = Random.Range(0f, 360f);
+            float total = _minSpinRounds * 360f + randomEndAngle;
+
+            _wheelBase.transform.DORotate(new Vector3(0f, 0f, -total), _spinDuration, RotateMode.FastBeyond360)
+                .SetEase(Ease.OutCubic)
+                .SetLink(gameObject)
+                .OnComplete(() =>
+                {
+                _isSpinning = false;
+                _spinButton.interactable = true; 
+                });
+        }
     }
 }
