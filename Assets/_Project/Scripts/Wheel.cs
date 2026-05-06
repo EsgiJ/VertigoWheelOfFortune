6f1e52b1169ac7b6fab5e801bbe3c59b6e23290e
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2026-05-06 17:55:30 +0300

    feat(wheel script): add basic wheel spin logic

--- Assets/_Project/Scripts/Wheel.cs
+++ Assets/_Project/Scripts/Wheel.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class Wheel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private RectTransform _wheelBase;
+    [SerializeField] private Button _spinButton;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float _spinDuration = 4f;
+    [SerializeField] private int _minSpinRounds= 3;
+
+    private bool _isSpinning = false;
+
+    void Awake()
     {
-        
+        _spinButton.onClick.AddListener(Spin);
+    }
+    
+    private void Spin()
+    {
+        if(_isSpinning)
+            return;
+
+        _isSpinning = true;
+        _spinButton.interactable = false;
+
+        float randomEndAngle = Random.Range(0f, 360f);
+        float total = _minSpinRounds * 360f + randomEndAngle;
+
+        _wheelBase.transform.DORotate(new Vector3(0f, 0f, -total), _spinDuration, RotateMode.FastBeyond360)
+            .SetEase(Ease.OutCubic)
+            .SetLink(gameObject)
+            .OnComplete(() =>
+            {
+               _isSpinning = false;
+               _spinButton.interactable = true; 
+            });
     }
 }
