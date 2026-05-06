f06373a1d68ccc1252d25fbf614940ff3ecff37a
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2026-05-06 22:57:10 +0300

    feat(rewards): add RewardData scriptable object

---
+++ Assets/_Project/Scripts/RewardData.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = = "RewardData", menuName = "WheelOfFortune/RewardData")]
+public class RewardData : ScriptableObject
+{
+    [SerializeField] private string _displayName;
+    [SerializeField] private Sprite _icon;
+    [SerializeField] private int _baseAmount;
+
+    public string DisplayName => _displayName;
+    public Sprite Icon => _icon;       
+    public int BaseAmount => _baseAmount;
+}
