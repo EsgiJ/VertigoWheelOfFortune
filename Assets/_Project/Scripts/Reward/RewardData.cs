4ea62728c1ad5c33f6a0dd81519571a0af76e4c5
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2026-05-07 17:04:24 +0300

    feat: add isStackable field to RewardData

--- Assets/_Project/Scripts/Reward/RewardData.cs
+++ Assets/_Project/Scripts/Reward/RewardData.cs
@@ -9,9 +9,11 @@ namespace WheelOfFortune.Reward
         [SerializeField] private string _displayName = "Default";
         [SerializeField] private Sprite _icon = null;
         [SerializeField] private int _baseAmount = 1;
+        [SerializeField] private bool _isStackable = true;
 
         public string DisplayName => _displayName;
         public Sprite Icon => _icon;       
         public int BaseAmount => _baseAmount;
+        public bool IsStackable => _isStackable;
     }
 }
