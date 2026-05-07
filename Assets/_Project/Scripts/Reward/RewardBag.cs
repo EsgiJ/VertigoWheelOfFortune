e229dca76d55911148415f0ff544701c8d938da6
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2026-05-07 17:05:04 +0300

    feat: add AddReward and Clear methods to RewardBag

--- Assets/_Project/Scripts/Reward/RewardBag.cs
+++ Assets/_Project/Scripts/Reward/RewardBag.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RewardBag : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace WheelOfFortune.Reward
+{   
+    public class RewardBag : MonoBehaviour
     {
-        
-    }
+        private readonly List<CollectedReward> _collectedRewards = new List<CollectedReward>();
+        public IReadOnlyList<CollectedReward> CollectedRewards => _collectedRewards;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void AddReward(RewardData rewardData, int amount)
+        {
+            if (rewardData == null || amount <= 0)
+                return;
+            
+            if(rewardData.IsStackable)
+            {
+                for(int i = 0; i < _collectedRewards.Count; i++)
+                {
+                    if(_collectedRewards[i].RewardData == rewardData)
+                    {
+                        int newAmount = _collectedRewards[i].Amount + amount;
+                        CollectedReward mergedReward = new CollectedReward(rewardData, newAmount);
+                        _collectedRewards[i] = mergedReward;
+                        return;
+                    }
+                }
+            }
+            CollectedReward newReward = new CollectedReward(rewardData, amount);
+            _collectedRewards.Add(newReward);
+        }
+
+        public void ClearRewards()
+        {
+            _collectedRewards.Clear();
+        }
     }
 }
