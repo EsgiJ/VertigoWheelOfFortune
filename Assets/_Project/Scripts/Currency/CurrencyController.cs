c001649cb224589ad7b455e6361ff2a3e87c845b
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2026-05-08 20:08:00 +0300

    feat(currency): create CurrencyController script

---
+++ Assets/_Project/Scripts/Currency/CurrencyController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrencyController : MonoBehaviour
+{
+    [SerializeField, Min(0)] private int _startingCurrency = 100;
+
+    public int Gold { get; private set; }   
+
+    public event Action<int> OnCurrencyChanged;
+
+    private void Awake()
+    {
+        Gold = _startingCurrency;
+    }
+
+    private void Start()
+    {
+        OnCurrencyChanged?.Invoke(Gold);
+    }
+
+    public bool CanAfford(int amount)
+    {
+        return Gold >= amount;
+    }
+
+    public bool TrySpend(int amount)
+    {
+        if(CanAfford(amount))
+        {
+            Gold -= amount;
+            OnCurrencyChanged?.Invoke(Gold);
+            return true;
+        }
+
+        return false;
+    }
+}
