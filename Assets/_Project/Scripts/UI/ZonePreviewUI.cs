786e7f8d7fb6a64bfda8e6dc3b7a5396e2d35c64
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2026-05-11 22:46:28 +0300

    fix: add namespace to ZonePreviewUI script

--- Assets/_Project/Scripts/UI/ZonePreviewUI.cs
+++ Assets/_Project/Scripts/UI/ZonePreviewUI.cs
@@ -4,59 +4,62 @@ using UnityEngine;
 using WheelOfFortune.Zone;
 using WheelOfFortune.Core;
 
-public class ZonePreviewUI : MonoBehaviour
+namespace WheelOfFortune.UI
 {
-    [Header("References")]
-    [SerializeField] private TMP_Text _uiTextSuperValue;     
-    [SerializeField] private TMP_Text _uiTextSafeValue;      
-    [SerializeField] private ZoneController _zoneController;
-
-    [Header("Config")]
-    [SerializeField] private GameConfig _gameConfig;
-    
-    private void OnEnable()
+    public class ZonePreviewUI : MonoBehaviour
     {
-        _zoneController.OnZoneChanged += HandleZoneChanged;
-    }
+        [Header("References")]
+        [SerializeField] private TMP_Text _uiTextSuperValue;     
+        [SerializeField] private TMP_Text _uiTextSafeValue;      
+        [SerializeField] private ZoneController _zoneController;
 
-    private void OnDisable()
-    {
-        _zoneController.OnZoneChanged -= HandleZoneChanged;
-    }
-
-    private void HandleZoneChanged(int currentZone, ZoneType _)
-    {
-        int nextSuper = _gameConfig.GetNextSuperZone(currentZone);
-        int nextSafe  = _gameConfig.GetNextSafeZone(currentZone);
-
-        _uiTextSuperValue.text = $"SUPER ZONE {nextSuper}";
-        _uiTextSafeValue.text  = $"SAFE ZONE {nextSafe}";
-    }
-
-#if UNITY_EDITOR
-    private void OnValidate()
-    {
-        if (_uiTextSuperValue == null)
+        [Header("Config")]
+        [SerializeField] private GameConfig _gameConfig;
+        
+        private void OnEnable()
         {
-            var t = transform.Find("ui_panel_zone_preview_super/ui_text_zone_label_value");
-            if (t != null) _uiTextSuperValue = t.GetComponent<TMP_Text>();
+            _zoneController.OnZoneChanged += HandleZoneChanged;
         }
 
-        if (_uiTextSafeValue == null)
+        private void OnDisable()
         {
-            var t = transform.Find("ui_panel_zone_preview_safe/ui_text_zone_label_value");
-            if (t != null) _uiTextSafeValue = t.GetComponent<TMP_Text>();
+            _zoneController.OnZoneChanged -= HandleZoneChanged;
         }
 
-        if (_gameConfig == null)
+        private void HandleZoneChanged(int currentZone, ZoneType _)
         {
-            var guids = UnityEditor.AssetDatabase.FindAssets("t:GameConfig");
-            if (guids.Length > 0)
+            int nextSuper = _gameConfig.GetNextSuperZone(currentZone);
+            int nextSafe  = _gameConfig.GetNextSafeZone(currentZone);
+
+            _uiTextSuperValue.text = $"SUPER ZONE {nextSuper}";
+            _uiTextSafeValue.text  = $"SAFE ZONE {nextSafe}";
+        }
+
+    #if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (_uiTextSuperValue == null)
             {
-                string path = UnityEditor.AssetDatabase.GUIDToAssetPath(guids[0]);
-                _gameConfig = UnityEditor.AssetDatabase.LoadAssetAtPath<GameConfig>(path);
+                var t = transform.Find("ui_panel_zone_preview_super/ui_text_zone_label_value");
+                if (t != null) _uiTextSuperValue = t.GetComponent<TMP_Text>();
+            }
+
+            if (_uiTextSafeValue == null)
+            {
+                var t = transform.Find("ui_panel_zone_preview_safe/ui_text_zone_label_value");
+                if (t != null) _uiTextSafeValue = t.GetComponent<TMP_Text>();
+            }
+
+            if (_gameConfig == null)
+            {
+                var guids = UnityEditor.AssetDatabase.FindAssets("t:GameConfig");
+                if (guids.Length > 0)
+                {
+                    string path = UnityEditor.AssetDatabase.GUIDToAssetPath(guids[0]);
+                    _gameConfig = UnityEditor.AssetDatabase.LoadAssetAtPath<GameConfig>(path);
+                }
             }
         }
+    #endif
     }
-#endif
 }
